b0071632ac547bdb0424936caf3f06b0d89184ca
Maxim Fominykh <vominyh@yandex.ru>
2020-01-16 22:40:38 +0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,2 +1,6 @@
-sudo apt install -y git-core zsh fonts-powerline && sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins && source ~/.zshrc
+sudo apt install -y git-core zsh fonts-powerline;
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh) -y";
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting; 
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions;
 sed -i 's/(git)/(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+source ~/.zshrc;
