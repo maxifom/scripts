b5732074ec2d0cff4f43dfe80e9676a790709583
Maxim Fominykh <fominykh.max@gmail.com>
2021-06-01 22:50:55 +0500

    Update kube.sh

--- kube.sh
+++ kube.sh
@@ -7,7 +7,6 @@ echo "$(<kubectl.sha256) kubectl" | sha256sum --check;
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl;
 kubectl version --client;
 
-wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz;
-
-tar -zxvf helm-v3.6.0-linux-amd64.tar.gz;
-mv linux-amd64/helm /usr/local/bin/helm;
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3;
+chmod 700 get_helm.sh;
+./get_helm.sh;
