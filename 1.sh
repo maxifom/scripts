d1e2f3662fe52cef1aeb7ac63b2a5152e5b7587f
Maxim Fominykh <fominykh.max@gmail.com>
2021-06-01 22:44:10 +0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,11 +1,25 @@
-sudo apt install -y git-core zsh fonts-powerline;
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh) -y";
+#!/bin/bash
+
+set -ex;
+apt update -y && apt upgrade -y;
+apt install -y git-core zsh fonts-powerline wget curl htop;
+wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh;
+chmod +x ./install.sh;
+./install.sh -y;
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting; 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions;
 sed -i 's/(git)/(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
 source ~/.zshrc;
-chsh -s $(which zsh);
-curl -fsSL https://get.docker.com -o get-docker.sh;
-sh get-docker.sh;
-rm get-docker.sh;
+wget -O docker.sh https://get.docker.com;
+chmod +x ./docker.sh;
+./docker.sh;
+rm docker.sh install.sh;
+echo "DONE";
+
+
+
+
+
+
+
 
