a91db5a6851078c146b40d50b4f17f1a2c2c6057
Maxim Fominykh <fominykh.max@gmail.com>
2021-06-02 18:02:05 +0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -5,7 +5,7 @@ apt update -y && apt upgrade -y;
 apt install -y git-core zsh fonts-powerline wget curl htop;
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh;
 chmod +x ./install.sh;
-yes | ./install.sh;
+./install.sh --unattended;
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting; 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions;
 sed -i 's/(git)/(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
