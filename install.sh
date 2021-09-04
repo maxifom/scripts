6287d69aab30ad1fc6708c249c7d545bf593d69a
Maxim Fominykh <fominykh.max@gmail.com>
2021-09-04 19:06:57 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/cust
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions;
 sed -i 's/(git)/(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
 source ~/.zshrc;
-wget -O docker.sh https://get.docker.com;
-chmod +x ./docker.sh;
-./docker.sh;
-rm docker.sh zsh.sh;
-echo "DONE";
+wget -O- docker.sh https://get.docker.com | sh;
